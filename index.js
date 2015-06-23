261f4aca4d5060f3e448cdb20f92709f2bdd1ff4
Matthew Edmond <medmond95@gmail.com>
2015-06-23 12:19:51 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+ var myData = require('./data.json');
+
+
+
+// Main routing
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/api', function(req, res){
+res.json(myData);
+});
+
+app.get('/api/:id', function(req, res){
+  console.log('Require:', req.params.id);
+  res.json(myData[req.params.id]);
+  });
+
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next) {
+  res.status(404);
+  res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.');
+});
